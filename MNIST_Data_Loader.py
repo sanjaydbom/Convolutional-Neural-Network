33790d8b143b9ceb2b940fd003e579a120dcb32c
sanjaydbom <64293324+sanjaydbom@users.noreply.github.com>
2024-10-08 17:18:31 -0700

    Update MNIST_Data_Loader.py

--- MNIST_Data_Loader.py
+++ MNIST_Data_Loader.py
@@ -1,18 +1,18 @@
 import numpy as np
 import struct
-import sys
-import time
+from array import array
 
 class MNIST_Data_Loader():
-    def __init__(self):
-        self.training_data_labels_file = open("/Users/sanjay/Downloads/archive/train-labels.idx1-ubyte", "rb")
-        self.training_data_images_file = open("/Users/sanjay/Downloads/archive/train-images.idx3-ubyte", "rb")
-        self.test_data_labels_file = open("/Users/sanjay/Downloads/archive/t10k-labels.idx1-ubyte", "rb")
-        self.test_data_images_file = open("/Users/sanjay/Downloads/archive/t10k-images.idx3-ubyte", "rb")
-        #self.training_data_labels = self.unpack(self.training_data_labels_file, 2049)
-        #self.training_data_images = self.unpack(self.training_data_images_file, 2051)
+    def __init__(self, training_data_lable_filepath,training_data_images_filepath,test_data_labels_filepath,test_data_images_filepath):
+        self.training_data_labels_file = open(training_data_lable_filepath, "rb")
+        self.training_data_images_file = open(training_data_images_filepath, "rb")
+        self.test_data_labels_file = open(test_data_labels_filepath, "rb")
+        self.test_data_images_file = open(test_data_images_filepath, "rb")
+
+        self.training_data_labels = self.unpack(self.training_data_labels_file, 2049)
+        self.training_data_images = self.unpack(self.training_data_images_file, 2051)
         self.test_data_labels = self.unpack(self.test_data_labels_file, 2049)
-        #self.test_data_images = self.unpack(self.test_data_images_file, 2051)
+        self.test_data_images = self.unpack(self.test_data_images_file, 2051)
 
         self.training_data_labels_file.close()
         self.training_data_images_file.close()
@@ -23,14 +23,17 @@ class MNIST_Data_Loader():
         MSB, num_items = struct.unpack(">II",name.read(8))
         if(MSB != expected_MSB):
             raise ValueError(f"Expected {expected_MSB}, recieved {MSB}")
+        
         if expected_MSB == 2051:
             num_rows, num_cols = struct.unpack(">II", name.read(8))
-            print(f"number of rows: {num_rows}\nnumber of cols: {num_cols}")
-            array =  name.read()
-            images = np.array([[[[array[num_rows*num_cols*num_image + j * 28 + i]] for i in range(num_cols)] for j in range(num_rows)] for num_image in range(num_items)])
+            unordered_data = array("B", name.read())
+            images = [0] * num_items
+            for i in range(num_items):
+                images[i] = np.reshape(unordered_data[i * num_rows * num_cols : (i + 1) * num_rows * num_cols],(28,28))
             return images
-        testdi = name.unpack()
-        print(testdi)
-        return testdi
-
-m = MNIST_Data_Loader()
+        
+        labels = np.array(array("B", name.read()))
+        return labels
+    
+    def get_data(self):
+        return(self.training_data_images, self.training_data_labels), (self.test_data_images, self.test_data_labels)
